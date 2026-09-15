c3b55edf88d72a3993b2c798a98906978eb91437
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-09-14 22:04:25 -0300

    chore(web): integrate Redux for state management and enable CORS in server

--- apps/server/src/infra/main.ts
+++ apps/server/src/infra/main.ts
@@ -10,6 +10,7 @@ import { setupSwagger } from '@infra/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.enableShutdownHooks();
   app.useGlobalPipes(new StandardSchemaValidationPipe());
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import './index.css'
 
 import { AppRoutes } from './routes.tsx'
+import { apiStore } from './store/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRoutes />
+    <Provider store={apiStore}>
+      <AppRoutes />
+    </Provider>
   </StrictMode>,
 )
