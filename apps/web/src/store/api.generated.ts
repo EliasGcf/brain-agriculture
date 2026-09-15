f6136d485a2d7020eed3ee55b9cfe4ed5a5cea84
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-09-14 21:42:51 -0300

    chore(web): configure RTK Query OpenAPI codegen

---
+++ apps/web/src/store/api.generated.ts
@@ -0,0 +1,297 @@
+import { api } from "./api";
+const injectedRtkApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    createProducer: build.mutation<
+      CreateProducerApiResponse,
+      CreateProducerApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/producers`,
+        method: "POST",
+        body: queryArg.body,
+      }),
+    }),
+    listProducers: build.query<ListProducersApiResponse, ListProducersApiArg>({
+      query: (queryArg) => ({
+        url: `/producers`,
+        params: {
+          name: queryArg.name,
+          document: queryArg.document,
+          page: queryArg.page,
+          perPage: queryArg.perPage,
+        },
+      }),
+    }),
+    getProducerById: build.query<
+      GetProducerByIdApiResponse,
+      GetProducerByIdApiArg
+    >({
+      query: (queryArg) => ({ url: `/producers/${queryArg.id}` }),
+    }),
+    updateProducer: build.mutation<
+      UpdateProducerApiResponse,
+      UpdateProducerApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/producers/${queryArg.id}`,
+        method: "PATCH",
+        body: queryArg.body,
+      }),
+    }),
+    createFarm: build.mutation<CreateFarmApiResponse, CreateFarmApiArg>({
+      query: (queryArg) => ({
+        url: `/farms`,
+        method: "POST",
+        body: queryArg.body,
+      }),
+    }),
+    getFarmById: build.query<GetFarmByIdApiResponse, GetFarmByIdApiArg>({
+      query: (queryArg) => ({ url: `/farms/${queryArg.id}` }),
+    }),
+    updateFarm: build.mutation<UpdateFarmApiResponse, UpdateFarmApiArg>({
+      query: (queryArg) => ({
+        url: `/farms/${queryArg.id}`,
+        method: "PATCH",
+        body: queryArg.body,
+      }),
+    }),
+    deleteFarm: build.mutation<DeleteFarmApiResponse, DeleteFarmApiArg>({
+      query: (queryArg) => ({ url: `/farms/${queryArg.id}`, method: "DELETE" }),
+    }),
+    createHarvest: build.mutation<
+      CreateHarvestApiResponse,
+      CreateHarvestApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/harvests`,
+        method: "POST",
+        body: queryArg.body,
+      }),
+    }),
+    getHarvestById: build.query<
+      GetHarvestByIdApiResponse,
+      GetHarvestByIdApiArg
+    >({
+      query: (queryArg) => ({ url: `/harvests/${queryArg.id}` }),
+    }),
+    updateHarvest: build.mutation<
+      UpdateHarvestApiResponse,
+      UpdateHarvestApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/harvests/${queryArg.id}`,
+        method: "PATCH",
+        body: queryArg.body,
+      }),
+    }),
+    deleteHarvest: build.mutation<
+      DeleteHarvestApiResponse,
+      DeleteHarvestApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/harvests/${queryArg.id}`,
+        method: "DELETE",
+      }),
+    }),
+    createPlantedCrop: build.mutation<
+      CreatePlantedCropApiResponse,
+      CreatePlantedCropApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/planted-crops`,
+        method: "POST",
+        body: queryArg.body,
+      }),
+    }),
+    getPlantedCropById: build.query<
+      GetPlantedCropByIdApiResponse,
+      GetPlantedCropByIdApiArg
+    >({
+      query: (queryArg) => ({ url: `/planted-crops/${queryArg.id}` }),
+    }),
+    updatePlantedCrop: build.mutation<
+      UpdatePlantedCropApiResponse,
+      UpdatePlantedCropApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/planted-crops/${queryArg.id}`,
+        method: "PATCH",
+        body: queryArg.body,
+      }),
+    }),
+    deletePlantedCrop: build.mutation<
+      DeletePlantedCropApiResponse,
+      DeletePlantedCropApiArg
+    >({
+      query: (queryArg) => ({
+        url: `/planted-crops/${queryArg.id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+  overrideExisting: false,
+});
+export { injectedRtkApi as api };
+export type CreateProducerApiResponse = /** status 201  */ ProducerResponse;
+export type CreateProducerApiArg = {
+  body: {
+    name: string;
+    document: string;
+  };
+};
+export type ListProducersApiResponse =
+  /** status 200  */ PaginatedProducerResponse;
+export type ListProducersApiArg = {
+  name?: string;
+  document?: string;
+  page?: number;
+  perPage?: number;
+};
+export type GetProducerByIdApiResponse = /** status 200  */ ProducerResponse;
+export type GetProducerByIdApiArg = {
+  id: string;
+};
+export type UpdateProducerApiResponse = /** status 200  */ ProducerResponse;
+export type UpdateProducerApiArg = {
+  id: string;
+  body: {
+    name?: string;
+    document?: string;
+  };
+};
+export type CreateFarmApiResponse = /** status 201  */ FarmResponse;
+export type CreateFarmApiArg = {
+  body: {
+    name: string;
+    producerId: string;
+    city: string;
+    state: string;
+    totalArea: number;
+    arableArea: number;
+    vegetationArea: number;
+  };
+};
+export type GetFarmByIdApiResponse = /** status 200  */ FarmResponse;
+export type GetFarmByIdApiArg = {
+  id: string;
+};
+export type UpdateFarmApiResponse = /** status 200  */ FarmResponse;
+export type UpdateFarmApiArg = {
+  id: string;
+  body: {
+    name?: string;
+    producerId?: string;
+    city?: string;
+    state?: string;
+    totalArea?: number;
+    arableArea?: number;
+    vegetationArea?: number;
+  };
+};
+export type DeleteFarmApiResponse = unknown;
+export type DeleteFarmApiArg = {
+  id: string;
+};
+export type CreateHarvestApiResponse = /** status 201  */ HarvestResponse;
+export type CreateHarvestApiArg = {
+  body: {
+    name: string;
+    farmId: string;
+  };
+};
+export type GetHarvestByIdApiResponse = /** status 200  */ HarvestResponse;
+export type GetHarvestByIdApiArg = {
+  id: string;
+};
+export type UpdateHarvestApiResponse = /** status 200  */ HarvestResponse;
+export type UpdateHarvestApiArg = {
+  id: string;
+  body: {
+    name: string;
+  };
+};
+export type DeleteHarvestApiResponse = unknown;
+export type DeleteHarvestApiArg = {
+  id: string;
+};
+export type CreatePlantedCropApiResponse =
+  /** status 201  */ PlantedCropResponse;
+export type CreatePlantedCropApiArg = {
+  body: {
+    name: string;
+    harvestId: string;
+  };
+};
+export type GetPlantedCropByIdApiResponse =
+  /** status 200  */ PlantedCropResponse;
+export type GetPlantedCropByIdApiArg = {
+  id: string;
+};
+export type UpdatePlantedCropApiResponse =
+  /** status 200  */ PlantedCropResponse;
+export type UpdatePlantedCropApiArg = {
+  id: string;
+  body: {
+    name: string;
+  };
+};
+export type DeletePlantedCropApiResponse = unknown;
+export type DeletePlantedCropApiArg = {
+  id: string;
+};
+export type ProducerResponse = {
+  id: string;
+  name: string;
+  document: string;
+  createdAt: string;
+};
+export type PaginatedProducerResponse = {
+  items: {
+    id: string;
+    name: string;
+    document: string;
+    createdAt: string;
+  }[];
+  total: number;
+};
+export type FarmResponse = {
+  id: string;
+  name: string;
+  producerId: string;
+  city: string;
+  state: string;
+  totalArea: number;
+  arableArea: number;
+  vegetationArea: number;
+  createdAt: string;
+};
+export type HarvestResponse = {
+  id: string;
+  name: string;
+  farmId: string;
+  createdAt: string;
+};
+export type PlantedCropResponse = {
+  id: string;
+  name: string;
+  harvestId: string;
+  createdAt: string;
+};
+export const {
+  useCreateProducerMutation,
+  useListProducersQuery,
+  useGetProducerByIdQuery,
+  useUpdateProducerMutation,
+  useCreateFarmMutation,
+  useGetFarmByIdQuery,
+  useUpdateFarmMutation,
+  useDeleteFarmMutation,
+  useCreateHarvestMutation,
+  useGetHarvestByIdQuery,
+  useUpdateHarvestMutation,
+  useDeleteHarvestMutation,
+  useCreatePlantedCropMutation,
+  useGetPlantedCropByIdQuery,
+  useUpdatePlantedCropMutation,
+  useDeletePlantedCropMutation,
+} = injectedRtkApi;
