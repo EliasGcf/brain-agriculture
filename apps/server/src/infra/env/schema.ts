5abbfad97b88cc8b7164ff9afa91ebe940205121
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-09-15 16:53:27 -0300

    feat(server): configure pino logger (#17)

--- apps/server/src/infra/env/schema.ts
+++ apps/server/src/infra/env/schema.ts
@@ -4,7 +4,7 @@ export const EnvSchema = z.object({
   DATABASE_URL: z.url(),
   PORT: z.coerce.number().optional().default(3333),
   NODE_ENV: z
-    .enum(['development', 'production', 'test'])
+    .enum(['development', 'test', 'production'])
     .optional()
     .default('development'),
 });
--- apps/server/src/infra/main.ts
+++ apps/server/src/infra/main.ts
@@ -2,13 +2,16 @@ import fs from 'node:fs/promises';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { StandardSchemaValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NativeLogger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 import { EnvService } from '@infra/env/env.service';
 import { setupSwagger } from '@infra/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(NativeLogger));
 
   app.enableCors();
   app.enableShutdownHooks();
