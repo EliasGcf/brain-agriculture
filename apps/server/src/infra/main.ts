e7825f7c6d8a2321598821bcc23622f36c4c1361
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-09-14 21:46:36 -0300

    chore(web): add OpenAPI documentation and setup Swagger integration

--- apps/server/src/infra/main.ts
+++ apps/server/src/infra/main.ts
@@ -1,9 +1,11 @@
+import fs from 'node:fs/promises';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { StandardSchemaValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { EnvService } from '@infra/env/env.service';
+import { setupSwagger } from '@infra/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,17 +13,9 @@ async function bootstrap() {
   app.enableShutdownHooks();
   app.useGlobalPipes(new StandardSchemaValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('Brain Agriculture')
-    .setDescription('API documentation for Brain Agriculture')
-    .setVersion('1.0')
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('docs', app, documentFactory);
-
   const env = app.get(EnvService);
+
+  setupSwagger(app, env);
 
   await app.listen(env.get('PORT'));
 }
